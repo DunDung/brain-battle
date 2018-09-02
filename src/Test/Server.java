20bc1858673c940c84cba6a728dfab434928a57a
JunSeong Hong <33235211+DunDung@users.noreply.github.com>
2018-09-03 00:46:26 +0900

    0903
    .

--- src/Test/Server.java
+++ src/Test/Server.java
@@ -1,5 +1,11 @@
 package Test;
 
-public class Server {
+import java.net.ServerSocket;
 
+public class Server {
+	int portNumber; //포트번호
+	int queueLength; //서버가 연결되길 기다리는 클라이언트의 최대 개수
+	ServerSocket server = new ServerSocket(portNumber, queueLength); //ServerSocket 객체 생성
+	
+	
 }
