7f84be25d8c96af2b679148f29dcd33dd1534593
JunSeong Hong <33235211+DunDung@users.noreply.github.com>
2018-09-05 13:05:15 +0900

    0905

--- src/client/MainServer.java
+++ src/client/MainServer.java
@@ -7,7 +7,7 @@ import java.net.Socket;
 public class MainServer {
 	public static void main(String [] args) {
 		try {
-			Socket clientSocket =new Socket("192.168.61.137",8888); 
+			Socket clientSocket =new Socket("192.168.43.190",8888); 
 			
 			ReceiveThread re_thread = new ReceiveThread();
 			re_thread.setSocket(clientSocket);
