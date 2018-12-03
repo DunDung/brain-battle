a86081aecab64db8815ea6c1d2fea083dbdcf057
JunSeong Hong <33235211+DunDung@users.noreply.github.com>
2018-12-03 22:00:53 +0900

    1203
    주석 다는중.

--- src/thread/PreSettingThread.java
+++ src/thread/PreSettingThread.java
@@ -37,25 +37,23 @@ public class PreSettingThread extends Thread{
 			}
 
 			mainFrame.getChat().getTa().append("System :연결되었습니다! 먼저 게임설명을 읽어주세요.\nSystem :준비가 완료되면 Ready버튼을 눌러주세요.\n");
-
-			while(true) { //준비완료 
-				Thread.sleep(100);
-				if(mainFrame.getGame().getPlayOk()) {
-					writer.println("ready/");
-					mainFrame.getChat().getTa().append("System :준비완료!\n");
-					break;
+			
+			while(true) { //준비완료  버튼을 누를 때 까지 계속 돈다.
+				Thread.sleep(100); 
+				if(mainFrame.getGame().getPlayOk()) { //사용자가 준비완료 버튼을 눌러서 playOk가 true가 됐을 경우
+					writer.println("ready/"); //상대에게 ready 키워드를 보낸다.
+					mainFrame.getChat().getTa().append("System :준비완료!\n"); //채팅창에 준비완료가 처리됐음을 알린다. 
+					break; //while문 빠져나감
 				}
 			}
-			
 		} catch (IOException  | InterruptedException e) { 
 			e.printStackTrace(); 
 		} 
-
 	}
+	
 	class SendEvent implements ActionListener{ //enter키를 누를 때와 "전송"버튼을 눌렀을 때의 이벤트 클래스
 		@Override
 		public void actionPerformed(ActionEvent e) { 
-
 			String 	sendString =mainFrame.getChat().getTf().getText(); //택스트 필드에 있는 문자열을 sendString에 저장한다.
 			writer.println("chat/"+mainFrame.getScore().getMyNickName() +" :"+ sendString ); //소켓의 아웃풋스트림에 sendString을 보낸다.
 			mainFrame.getChat().getTa().append(mainFrame.getScore().getMyNickName() +" :"+ sendString +"\n");  //JTextArea에 sendString을 추가한다.
